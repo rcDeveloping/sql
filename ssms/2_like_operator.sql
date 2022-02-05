80fcdbbffdf2cf38b0e3c501b7c5a00a20bf19aa
Robson Cruz <rcflorestal@yahoo.com.br>
2022-02-05 19:26:42 -0300

    like operator

--- ssms/2_like_operator.sql
+++ ssms/2_like_operator.sql
@@ -1,15 +1,3 @@
---Filtrar todos os clientes com o sobrenome MCdonald
-SELECT
-	CONCAT(FirstName, ' ',	LastName) AS Name,
-	EmailAddress,
-	Phone
-	
-FROM DimCustomer
-
-WHERE LastName LIKE 'MCdonald'
-
-ORDER BY FirstName;
-
 -- Filtrar todos os clientes cujo primeiro nome inicia e termina com a letra "A"
 SELECT
 	CONCAT(FirstName, ' ',	LastName) AS Name,
@@ -45,3 +33,11 @@ FROM DimCustomer
 WHERE FirstName LIKE 'C_r%'
 
 ORDER BY FirstName;
+
+--Filtrar a coluna "ProductDescription" da DimProduct, onde todos os produtos terminam com a descrição "WMA"
+SELECT * FROM DimProduct
+WHERE ProductDescription LIKE '%WMA';
+
+--Filtrar a coluna "ProductDescription" da tabela "DimProduct", onde todos os produtos começam com a descrição "4GB"
+SELECT * FROM DimProduct
+WHERE ProductDescription LIKE '4GB%';
