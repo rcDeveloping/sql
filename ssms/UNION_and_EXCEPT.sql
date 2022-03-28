50cf4cf60791948a3e2a5c6524bb234c3f069101
Robson Cruz <rcflorestal@yahoo.com.br>
2022-03-28 19:45:31 -0300

    Union and Except

---
+++ ssms/UNION_and_EXCEPT.sql
@@ -0,0 +1,23 @@
+/***
+Identify the country codes that are included in either 'economies' or 'currencies' but not in 'populations' table.
+***/ 
+
+-- Select the city name
+SELECT c1.name
+  -- Alias the table where city name resides
+  FROM cities AS c1
+  -- Choose only records matching the result of multiple set theory clauses
+  WHERE c1.country_code IN
+(
+    -- Select appropriate field from economies AS e
+    SELECT e.code
+    FROM economies AS e
+    -- Get all additional (unique) values of the field from currencies AS c2  
+    UNION
+    SELECT DISTINCT c2.code
+    FROM currencies AS c2
+    -- Exclude those appearing in populations AS p
+    EXCEPT
+    SELECT p.country_code
+    FROM populations AS p
+);
