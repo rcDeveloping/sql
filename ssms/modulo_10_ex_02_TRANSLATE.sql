2d5fb6b0b93916af8d2fc3b24987785915225e87
rcflorestal <rcflorestal@yahoo.com.br>
2022-05-06 20:40:52 -0300

    TRANSLATE sql server

---
+++ ssms/modulo_10_ex_02_TRANSLATE.sql
@@ -0,0 +1,12 @@
+/*** MÓDULO 10: EXERCÍCIOS
+2. A coluna StyleName da tabela DimProduct possui alguns códigos identificados por números distintos, que vão de 0 até 9.
+Porém, o setor de controle decidiu alterar a identificação do StyleName, e em vez de usar números, a ideia agora é passar a usar letras para substituir os números, conforme exemplo abaixo:
+0 -> A, 1 -> B, 2 -> C, 3 -> D, 4 -> E, 5 -> F, 6 -> G, 7 -> H, 8 -> I, 9 - J
+É de sua responsabilidade alterar os números por letras na coluna StyleName da tabela DimProduct. Utilize uma função que permita fazer essas substituições de forma prática e rápida.
+***/
+
+SELECT 
+	ProductName,
+	StyleName,
+	TRANSLATE(StyleName, '0123456789', 'ABCDEFGHIJ') AS NewStyleName
+FROM DimProduct;
