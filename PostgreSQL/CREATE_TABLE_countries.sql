e7d8ee3ecbce4bb42ffe752646d1efeb53a6a38d
Robson santos <rcflorestal@yahoo.com.br>
2022-05-10 10:47:22 -0300

    CREATE TABLE countries

---
+++ PostgreSQL/CREATE_TABLE_countries.sql
@@ -0,0 +1,13 @@
+CREATE TABLE countries(
+	code VARCHAR(50),
+	country_name VARCHAR(50),
+	continent VARCHAR(50),
+	region VARCHAR(50),
+	surface_area FLOAT,
+	indep_year SMALLINT,
+	local_name VARCHAR(50),
+	gov_form VARCHAR(50),
+	capital VARCHAR(50),
+	cap_long FLOAT,
+	cap_lat FLOAT
+)
