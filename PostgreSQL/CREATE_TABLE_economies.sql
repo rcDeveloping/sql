d9fffb8938bb9bf94872cb94adca12eb4fc04243
Robson santos <rcflorestal@yahoo.com.br>
2022-05-10 10:48:16 -0300

    CREATE TABLE economies

---
+++ PostgreSQL/CREATE_TABLE_economies.sql
@@ -0,0 +1,13 @@
+CREATE TABLE economies(
+	econ_id SMALLINT,
+	code VARCHAR(50),
+	year SMALLINT,
+	income_group VARCHAR(50),
+	gdp_percapita FLOAT,
+	gross_savings FLOAT,
+	inflation_rate FLOAT,
+	total_investment FLOAT,
+	unemployment_rate FLOAT,
+	exports FLOAT,
+	imports FLOAT
+)
