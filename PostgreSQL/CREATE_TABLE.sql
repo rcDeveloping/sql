36b136fa38b45373245070475c7c2373fbdbbe12
Robson santos <rcflorestal@yahoo.com.br>
2022-05-13 16:32:43 -0300

    Create tables with constraints

--- PostgreSQL/CREATE_TABLE.sql
+++ PostgreSQL/CREATE_TABLE.sql
@@ -1,18 +1,23 @@
 CREATE TABLE alunos(
 	ID_aluno INT,
-	Nome_Aluno VARCHAR(100),
-	Email VARCHAR(100)
+	Nome_Aluno VARCHAR(100) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	PRIMARY KEY(ID_aluno)
 );
 
 CREATE TABLE cursos(
 	ID_curso INT,
-	Nome_Curso VARCHAR(100),
-	Preco_Curso NUMERIC(10, 2)
+	Nome_Curso VARCHAR(100) NOT NULL,
+	Preco_Curso NUMERIC(10, 2) NOT NULL,
+	PRIMARY KEY(ID_curso)
 );
 
 CREATE TABLE matriculas(
 	ID_Matricula INT,
-	ID_Aluno INT,
-	ID_Curtso INT,
-	Data_Cadastro DATE
+	ID_Aluno INT NOT NULL,
+	ID_Curso INT NOT NULL,
+	Data_Cadastro DATE NOT NULL,
+	PRIMARY KEY(ID_Matricula),
+	FOREIGN KEY(ID_Aluno) REFERENCES alunos(ID_Aluno),
+	FOREIGN KEY(ID_Curso) REFERENCES cursos(ID_Curso)
 );
