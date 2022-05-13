e799a52f0cbac7a7e0176679be08ac4c6144dba7
Robson santos <rcflorestal@yahoo.com.br>
2022-05-13 14:15:22 -0300

    create table

---
+++ PostgreSQL/CREATE_DATABSE.sql
@@ -0,0 +1,12 @@
+-- Database: countries
+
+DROP DATABASE IF EXISTS countries;
+
+CREATE DATABASE countries
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
