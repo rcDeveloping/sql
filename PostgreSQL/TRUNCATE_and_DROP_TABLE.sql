3f866bfc8ce6331f338ff8cfc6062ae48fc9704f
Robson santos <rcflorestal@yahoo.com.br>
2022-05-16 20:47:25 -0300

    TRUNCATE and DROP TABLE - PostgreSQL

---
+++ PostgreSQL/TRUNCATE_and_DROP_TABLE.sql
@@ -0,0 +1,9 @@
+/***
+Remove todas as linhas de uma tabela ou partições especificadas de uma tabela, 
+porém mantém o tabela vazia no banco.
+***/
+TRUNCATE TABLE matriculas;
+
+DROP TABLE alunos CASCADE;
+DROP TABLE cursos;
+DROP TABLE matriculas;
