71df67a5d63373b4eaf6925e6842715adeb1b0a2
Robson santos <rcflorestal@yahoo.com.br>
2022-06-02 21:32:59 -0300

    Update Table

---
+++ ssms/modulo_13_ex_06_UPDATE_table.sql
@@ -0,0 +1,11 @@
+/*** Modulo 13 - CRUD 
+--> 5. 
+O contrato de ID igual a 4 foi registrado com alguns erros na tabela fContratos. Faça uma 
+alteração na tabela atualizando os seguintes valores:
+Data_de_Assinatura: 17/03/2019
+ID_Gerente: 2
+Valor_do_Contrato: 33500
+***/
+UPDATE fContratos
+SET Data_de_Assinatura = '17/03/2019', Valor_do_Contrato = 33500
+WHERE ID_Contrato = 4;
