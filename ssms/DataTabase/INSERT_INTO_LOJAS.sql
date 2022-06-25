b8e09219ed2b3d8360690e9438c8aa7421b7583f
Robson santos <rcflorestal@yahoo.com.br>
2022-06-25 09:59:23 -0300

    Insert into Lojas

---
+++ ssms/DataTabase/INSERT_INTO_LOJAS.sql
@@ -0,0 +1,19 @@
+USE WF
+GO
+
+INSERT INTO LOJAS (ID_Loja, Nome_Loja, Regiao, Qtd_Vendida)
+
+VALUES
+	(1, 'Botafogo Praia&Mar', 'Sudeste', 1800),
+	(2, 'Lojas Vitoria', 'Sudeste', 800),
+	(3, 'Empórioi Mineirinho', 'Sudeste', 2300),
+	(4, 'Central Paulista', 'Sudeste', 1800),
+	(5, 'Rio 90 graus', 'Sudeste', 700),
+	(6, 'Casa Flor & Anópolis', 'Sul', 2100),
+	(7, 'Pampas & Co', 'Sul', 990),
+	(8, 'Paraná Papéis', 'Sul', 2800),
+	(9, 'Amazonas Prime', 'Norte', 4200),
+	(10, 'Pará Bens', 'Norte', 3200),
+	(11, 'Tintas Rio Branco', 'Norte', 1500),
+	(12, 'Nordestemido Hall', 'Nordeste', 1910),
+	(13, 'Cachoerinha Loft', 'Nordeste', 2380)
