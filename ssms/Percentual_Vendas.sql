30e4b7e8ba76db690c5d91a92c25fadcab691a57
Robson santos <rcflorestal@yahoo.com.br>
2022-06-25 10:38:06 -0300

    Percentual de vendas

--- ssms/Percentual_Vendas.sql
+++ ssms/Percentual_Vendas.sql
@@ -9,3 +9,15 @@ SELECT
 	FORMAT(Qtd_Vendida / SUM(Qtd_Vendida) OVER(), '0.00%') AS '% do Total'
 FROM Lojas
 ORDER BY ID_Loja;
+
+--percentual de vendas por região
+SELECT 
+	ID_Loja,
+	Nome_Loja,
+	Regiao,
+	Qtd_Vendida,
+	SUM(Qtd_Vendida) OVER(PARTITION BY Regiao) AS 'Total Vendido',
+	--ROUND(Qtd_Vendida*100 / SUM(Qtd_Vendida) OVER(),2,2) AS '% do Total',
+	FORMAT(Qtd_Vendida / SUM(Qtd_Vendida) OVER(PARTITION BY Regiao), '0.00%') AS '% do Total'
+FROM Lojas
+ORDER BY Regiao;
